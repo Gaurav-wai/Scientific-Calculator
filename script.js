5f3dfcdeeb1e80fadd4930a6826d3ea92625e12c
Gaurav-wai <gaurav.tonpe@waiin.com>
2025-03-19 05:50:25 +0000

    final

---
+++ script.js
@@ -0,0 +1,71 @@
+console.log("hello");
+
+document.addEventListener("DOMContentLoaded", function(){
+const display = document.getElementById("calc-display");
+const buttons = document.getElementsByClassName('btn');
+console.log(display);
+console.log(buttons);
+
+// buttons[0].addEventListener("click", function(){
+//     console.log("button is clicked!")
+// })
+
+let currentValue = "";
+
+function evaluateResult(){
+    console.log( "currentValue: ", currentValue);
+    const convertedValue = currentValue
+        .replace("×", "*")
+        .replace("÷", "/")
+        .replace("%", "*0.01")
+        .replace("sin", "Math.sin")
+        .replace("cos", "Math.cos")
+        .replace("ln", "Math.log")
+        .replace("π", "Math.PI")
+        .replace("log", "Math.log10")
+        .replace("e", "Math.E")
+        .replace("tan", "Math.tan")
+        .replace("√","Math.sqrt");
+    
+    console.log("convertedValue: " , convertedValue)
+   
+    const result = eval(convertedValue);
+    currentValue = result.toString();
+    display.value = currentValue;
+}
+
+
+for(i=0 ; i<buttons.length; i++){
+    const button = buttons[i];
+    button.addEventListener("click", function(){
+        // console.log("clicked", button.innerText)
+        const value = button.innerText;
+        try{
+        if(value=="AC"){
+
+            currentValue="";
+            display.value = currentValue;
+        }else if(value == "="){
+            evaluateResult();
+        } else {
+            currentValue +=value;
+        
+        display.value = currentValue;
+        console.log("CurrentValue: "+currentValue);
+        }
+    } catch (error){
+        console.error(error);
+        currentValue="ERROR";
+        display.value=currentValue;
+    }
+    });
+
+
+
+
+
+}
+});
+
+
+
